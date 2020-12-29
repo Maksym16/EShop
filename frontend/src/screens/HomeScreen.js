c8adee8ed84cacb547611c9b868d24231a551ffb
Maksym <mfedorenko16@gmail.com>
2020-12-29 01:16:27 -0500

    added useState and effect to fetch data from the server

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import { Col, Row } from 'react-bootstrap';
 import Product from '../components/Product'
-import products from '../products'
 
 const HomeScreen = () => {
+  const [products, getProducts] = useState([])
+
+  useEffect(() => {
+    getAllProducts()
+  }, [])
+
+  const getAllProducts = async () => {
+    const products = await axios.get('/api/products')
+
+    if (products && products.data) {
+      getProducts(products.data);
+    }
+  }
   return (
     <>
       <h1>Latest Products</h1>
@@ -20,3 +33,6 @@ const HomeScreen = () => {
 }
 
 export default HomeScreen;
+
+  //will get error because back end and front end pointed to different points 3000 and 5000
+    // thats why we add proxy to jason file
