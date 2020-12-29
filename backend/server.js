236c9dc05a1defddbbe9b982d48e6119eea963e0
Maksym <mfedorenko16@gmail.com>
2020-12-29 01:37:33 -0500

    added env config

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express') //common js models system, import is ES models
 const products = require('./data/products')
-const app = express()
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+ const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running...')
@@ -15,4 +19,6 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product); //will convert arr of prodacts into json format
 });
 
-app.listen(5000, console.log('Server is running on 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
