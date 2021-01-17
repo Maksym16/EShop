d108e5bf6d983291501ad0248100d50375c45300
Maksym <mfedorenko16@gmail.com>
2021-01-16 21:48:43 -0500

    added error handler

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express' //common js models system, import is ES models
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import { notFound, errorHandler } from './middlewares/errorM.js';
 
 import productRoutes from './routes/productRoutes.js'
 
@@ -14,6 +15,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products/', productRoutes) //mount it, so if anything will go to products will mount to this router
+
+app.use(notFound);
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
