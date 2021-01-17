51438011d684972d5e08fc304708402097d3d067
Maksym <mfedorenko16@gmail.com>
2021-01-17 01:59:55 -0500

    added product reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers.js'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 
-const initialState = {} //used to put init values, which will be used while store is loading
+const initialState = {
+} //used to put init values, which will be used while store is loading
 
 const middleware = [thunk]
 
