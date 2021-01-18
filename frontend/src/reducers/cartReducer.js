99f0660e2b7da0c6b8579448b2d06b771f87b987
Maksym <mfedorenko16@gmail.com>
2021-01-17 21:42:23 -0500

    cart reducer & add to cart action

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,17 +1,20 @@
 import {
-  PRODUCT_LIST_REQUEST,
-  PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL,
-} from '../constants/productConstants.js';
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+} from '../constants/cartConstants';
 
-export const productListReducer = (state = { products: [] }, action) => {
+export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
-    case PRODUCT_LIST_REQUEST:
-      return { loading: true, product: [] };
-    case PRODUCT_LIST_SUCCESS:
-      return { loading: false, products: action.payload };
-    case PRODUCT_LIST_FAIL:
-      return { loading: false, error: action.payload };
+    case CART_ADD_ITEM:
+      const item = action.payload
+      const existItem = state.cartItems.find((x) => x.product === item)
+      if (existItem) {
+        return {...state, cartItems: state.cartItems.map((x) => x.product === existItem.product ? item : x)} //
+      } else {
+        return { ...state, cartItems: [...state.cartItems, item]}; //if item is not in a card already we will add it
+      }
+    // case CART_REMOVE_ITEM:
+    //   return { loading: false, products: action.payload };
     default:
       return state;
   }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,14 +2,19 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers.js'
+import { cartReducer } from './reducers/cartReducer.js'
+
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 const initialState = {
-} //used to put init values, which will be used while store is loading
+  cart: { cartItems: cartItemsFromStorage },
+}; //used to put init values, which will be used while store is loading
 
 const middleware = [thunk]
 
