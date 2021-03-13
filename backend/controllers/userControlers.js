e39c7a762507d2e78a8fdd22ffdabe72fedece55
Maksym <mfedorenko16@gmail.com>
2021-03-13 14:52:11 -0500

    added JWT token and authorization

--- backend/controllers/userControlers.js
+++ backend/controllers/userControlers.js
@@ -1,4 +1,5 @@
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js'
 import asyncHandler from 'express-async-handler';
 
 // @desc Auth user & get token
@@ -6,15 +7,16 @@ import asyncHandler from 'express-async-handler';
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const {email, password} = req.body;
-  const user = await User.findOne({ email });
-  console.log(User)
-  if (user && (await user.matchPassword(password))) {
+ 
+  const user = await User.findOne({ email }); //after we validate user with that email
+
+  if (user && (await user.matchPassword(password))) { //we want to match a password, if thats match we want to retorn user with that token
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401);
@@ -22,4 +24,23 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc Get user profile
+// @route  GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id); //after we validate user with that email
+
+  if (user) { //we want to match a password, if thats match we want to retorn user with that token
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404);
+    throw new Error('User not found')
+  }
+});
+
+export { authUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 const router = express.Router();
 import {
-  authUser
+  authUser, getUserProfile
 } from '../controllers/userControlers.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 // @desc Fetch all products
 // @route  GET /api/products
 // @access  Public
 
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile)
 
 export default router;
