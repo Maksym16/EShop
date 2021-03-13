24c327272db999ab3d881d18cc6c48bb82c7c07d
Maksym <mfedorenko16@gmail.com>
2021-03-13 15:28:02 -0500

    added user login reduser, and store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,8 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers.js'
+import { userLoginReducer } from './reducers/usersRedusers'
+
 import { cartReducer } from './reducers/cartReducer.js'
 
 
@@ -9,11 +11,14 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage},
 }; //used to put init values, which will be used while store is loading
 
 const middleware = [thunk]
