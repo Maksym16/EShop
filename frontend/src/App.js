1d5f574199e345d799913d51a35ce62ffdc1193e
Maksym <mfedorenko16@gmail.com>
2021-03-13 21:01:25 -0500

    added payment method select logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,9 +10,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
-
-
-
+import PaymentScreen from './screens/PaymentScreen';
 
 const App = () => {
   return (
@@ -22,6 +20,7 @@ const App = () => {
         <Container>
           <Route path="/" component={HomeScreen} exact />
           <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreen} />
           <Route path="/login" component={LoginScreen} />
           <Route path="/profile" component={ProfileScreen} />
           <Route path="/register" component={RegisterScreen} />
